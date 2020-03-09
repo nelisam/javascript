ab877af38508a69dedf0c8f106647d1d4c2e4ef9
nelisam <nelisamaqungo@gmail.com>
2020-03-09 13:24:29 +0200

    add alert folder and weather conditions

---
+++ js/alert.js
@@ -0,0 +1,30 @@
+//let weather = (prompt("Enter the weather"));
+// if (weather === "sunny") {
+//     console.log(weather + " The weather is sunny so put on some scandals and some shades");
+// } else if (weather === "rainy") {
+//      console.log(weather  + " The weather is rainy so put a raincoat or stay indoors");
+// } else if (weather === "snowy") {
+//      console.log(weather  + " The weather is snowy so stay indoors and get some hot chocolate");
+// } else if (weather === "windy") {
+//         console.log(weather  + " The weather is windy so put on a thick jacket");
+// } else {
+//     console.log(weather  + " Please enter one of these conditions : sunny, rainy, snowy, windy");
+// }
+
+
+
+let weather = (prompt("Enter the weather"));
+switch (weather) {
+    case ("sunny"):
+        console.log(weather + ", put on some scandals and some shades on");
+      break;
+    case ("rainy"):
+        console.log(weather  + ", put a raincoat or stay indoors");
+      break;
+    case ("snowy"):
+        console.log(weather  + ", stay indoors and get some hot chocolate");
+      break;
+    case ("windy"):
+        console.log(weather  + ", put on a thick jacket");
+      break;
+  }
---
+++ js/main.js
@@ -0,0 +1,45 @@
+// let message = "Good Morning";
+// console.log(message);
+
+// message = "How are you doing, today";
+// console.log(message);
+
+// let name = prompt ("Enter firstname");
+// alert(name+ message);
+
+// let name = prompt ("Enter firstname");
+// console.log(name + message);
+
+// message = " your age is ";
+// console.log(message);
+
+// let name = prompt("Enter your firstname");
+// let age = prompt("Enter your age");
+// console.log(name + message + age);
+
+
+// if statement
+// let number = Number(prompt("Enter a number"));
+// if(number < 0) {
+//     console.log(number  + " is negative");
+// }
+
+
+// // if else statement
+// let number = Number(prompt("Enter a number"));
+// if(number > 0) {
+//     console.log(number  + " is positive");
+// } else {
+//     console.log(number  + " is negative or zero");
+// }
+
+// Nesting Condition
+// let number = Number(prompt("Enter a number"));
+// if(number > 0) {
+//     console.log(number  + " is positive");
+// } else if (number < 0) {
+//     console.log(number  + " is negative");
+// } else {
+//     console.log(number  + " is zero");
+// }
+
