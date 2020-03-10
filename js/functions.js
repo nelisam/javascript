c6c90543eca14182568f60394d43427baef4edcd
nelisam <nelisamaqungo@gmail.com>
2020-03-10 11:57:03 +0200

    functions

---
+++ js/functions.js
@@ -0,0 +1,44 @@
+
+
+
+// function sayHello(){
+//     console.log("Hello");
+// }
+// sayHello();
+
+//declare function called theFunction
+// function theFunction() {
+//     function action;
+// }
+
+
+// function sayHello() {
+//     return "Hello!";
+// }
+
+
+// // to see the Hello in console
+// let result = sayHello();
+// console.log(result);
+
+// function sayHello() {
+
+//  }
+
+// let result = sayHello();
+// console.log(result);
+
+function sayHello(name, surname) {
+
+    var message =  "Hello," ;
+    return message + name+ " " +surname;
+}
+
+let name = prompt("Enter your name");
+let surname = prompt("Enter your surname");
+console.log(sayHello(name, surname));
+// console.log(sayHello("nelisa maqungo"));
+
+
+
+
