6374f80f4ad0488355da47806df7fe7d4b573659
nelisam <nelisamaqungo@gmail.com>
2020-03-10 11:06:03 +0200

    loops exercises

--- js/alert.js
+++ js/alert.js
@@ -5,7 +5,7 @@
 //      console.log(weather  + " The weather is rainy so put a raincoat or stay indoors");
 // } else if (weather === "snowy") {
 //      console.log(weather  + " The weather is snowy so stay indoors and get some hot chocolate");
-// } else if (weather === "windy") {
+// } else if (weather === "windy" &&  || ) {
 //         console.log(weather  + " The weather is windy so put on a thick jacket");
 // } else {
 //     console.log(weather  + " Please enter one of these conditions : sunny, rainy, snowy, windy");
@@ -13,18 +13,20 @@
 
 
 
-let weather = (prompt("Enter the weather"));
+let weather = prompt("Enter the weather");
 switch (weather) {
     case ("sunny"):
-        console.log(weather + ", put on some scandals and some shades on");
+        console.log(weather + ", put on some scandals and some shades on.");
       break;
     case ("rainy"):
-        console.log(weather  + ", put a raincoat or stay indoors");
+        console.log(weather  + ", put a raincoat or stay indoors.");
       break;
     case ("snowy"):
-        console.log(weather  + ", stay indoors and get some hot chocolate");
+        console.log(weather  + ", stay indoors and get some hot chocolate.");
       break;
     case ("windy"):
-        console.log(weather  + ", put on a thick jacket");
+        console.log(weather  + ", put on a thick jacket.");
       break;
+      default:
+    console.log("Please enter one of these conditions : sunny, rainy, snowy, windy");
   }
---
+++ js/loop.js
@@ -0,0 +1,39 @@
+// Loops
+
+// While
+
+
+// let number = 1;
+// while(number <= 1000) {
+//     console.log(number);
+//     number++;
+// }
+
+
+// For Loop
+
+// for(initialization; Condition;final - expression) {
+//     runs if condition is true;
+// }
+
+// for(let counter = 1; counter <= 5; counter++) {
+//     console.log(counter);
+// }
+// const numberOfPassengers = 10;
+
+// 
+// for(let i = 0; i <= numberOfPassengers; i++) {
+//     console.log(numberOfPassengers);
+// }
+
+// console.log('All passengers boarded')
+
+
+
+// List number of passengers from one - ten
+for(let passengers = 1; passengers  <= 10; passengers ++) {
+     console.log(passengers );
+}
+
+
+
